fc2cd1e6719d4f89031d9a6871d92988c4920ff9
Igor Kuritsyn <kio1996@mail.ru>
2016-10-28 00:25:29 +0400

    Update lab_2.cpp

--- lab_2.cpp
+++ lab_2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
-//Task 1 - Sorting
+//_______Task 1 - Sorting_______
 
 int A[]={31, 30, 69, 8, 74, 11, 40, 7, 48, 26, 65, 43, 73, 89, 44, 67, 41, 95, 55, 68};
 
@@ -28,4 +28,32 @@ int main()
 	system("pause");
 }
 
-//Task 2 - Bisection
+//_______Task 2 - Bisection_______
+
+float f(float x)
+{
+	return (exp(x)-cos(x)-1);
+}
+
+int main()
+{
+	float a=0,b=1,l,c,x=0;		// a,b - коннцы отрезка; c - середина отрезка; l - длина отрезка; x - искомая точка
+	l=b-a;
+	if (f(a)*f(b)<0)
+		cout<<"OK"<<endl;	// можем использовать метод бисекции
+	while (l>0.001)
+	{
+		l=l/2;
+		c=b-l;
+		x=c;
+		if (f(c)*f(b)<0)
+			a=c;
+		else if (f(a)*f(c)<0)
+			b=c;
+		else
+			break;
+	}
+	x=(float((int(x*10000))))/10000;		// округление в меньшую сторону с точночтью 0.0010
+	cout<<"f(x)=0 in x="<<x<<endl;
+	system("pause");
+}
